d8246fd1390f75540171d2b3846a38a3a94dea78
Isardy <dustygirardin@gmail.com>
2018-11-07 17:20:07 +0100

    Basic template

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Arma 3 Web Manager</title>
+		<meta name="description" content="A web manager for modded Arma 3 Dedicated servers.">
+		<meta name="author" content="Isardy">
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
+		<header class="index_header">
+			<h1>Arma 3 Web Manager</h1>
+		</header>
+		<section>
+			<?php include "login.php";?>
+		</section>
+
+
+
+	</body>
+</html>
---
+++ login.php
@@ -0,0 +1,16 @@
+<div class="loginform cf">
+	<form name="login" action="index_submit" method="get" accept-charset="utf-8">
+		<ul>
+			<li>
+				<label for="username">Username</label>
+				<input type="text" name="username" placeholder="username" required>
+			</li>
+			<li>
+				<label for="password">Password</label>
+				<input type="password" name="password" placeholder="password" required></li>
+				<li>
+					<input type="submit" value="Login">
+				</li>
+			</ul>
+		</form>
+</div>
